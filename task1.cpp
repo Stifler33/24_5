811ba75921f4bf76e106384e3483985a680fe4f4
stifler33 <bagi33.ru@gmail.com>
2021-10-16 20:46:18 +0300

    init task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -11,6 +11,12 @@ struct task{
     bool progress = false;
 };
 #define CHECK_EXIT if (userInput == "exit") return 0;
+#define BASE_PROGRESS baseTask.back().progress
+#define BASE_NAME baseTask.back().name
+#define BASE_TIME_S baseTask.back().TimeS
+#define BASE_START_TIME baseTask.back().startTime
+#define BASE_TIME_E  baseTask.back().TimeE
+#define BASE_END_TIME baseTask.back().endTime
 int main() {
     task strTask;
     std::vector<task> baseTask;
@@ -20,15 +26,30 @@ int main() {
         std::cin >> userInput;
         CHECK_EXIT
         if (userInput == "begin"){
-            int sizeBase = baseTask.size();
+            if(!baseTask.empty()){
+                if (BASE_PROGRESS){
+                    BASE_TIME_E = std::time(nullptr);
+                    BASE_END_TIME = std::localtime(&BASE_TIME_E);
+                    BASE_PROGRESS = false;
+                }
+            }
             std::cout << "Enter name task\n";
             std::cin >> userInput;
             baseTask.push_back(strTask);
-            baseTask.back().name = userInput;
-            baseTask.back().TimeS = std::time(nullptr);
-            baseTask.back().startTime = std::localtime(&baseTask.back().TimeS);
-            std::cout << "Added task " << "'" + baseTask.back().name + "'"<< std::endl;
-            std::cout << "Start time " << asctime(baseTask.back().startTime);
+            BASE_PROGRESS = true;
+            BASE_NAME = userInput;
+            BASE_TIME_S = std::time(nullptr);
+            BASE_START_TIME = std::localtime(&BASE_TIME_S);
+            std::cout << "Added task " << "'" + BASE_NAME + "'"<< std::endl;
+            std::cout << "Start time " << asctime(BASE_START_TIME);
+        }else if (userInput == "status"){
+            for (int i = 0; i < baseTask.size(); i++){
+                std::cout <<"'" + baseTask[i].name + "'"<< std::endl;
+                std::cout << "Start time " << asctime(baseTask[i].startTime) << std::endl;
+                if (!baseTask[i].progress) {
+                    std::cout << "End time " << asctime(baseTask[i].endTime) << std::endl;
+                }
+            }
         }
     }
     return 0;
