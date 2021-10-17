1e508b758fc22bed3691b610591a1a3c5acb88b1
stifler33 <bagi33.ru@gmail.com>
2021-10-17 16:29:57 +0300

    task1 finish

--- task1.cpp
+++ task1.cpp
@@ -4,19 +4,17 @@
 #include <iomanip>
 struct task{
     std::string name;
-    std::time_t TimeS;
-    std::tm* startTime;
-    std::time_t TimeE;
-    std::tm* endTime;
+    std::time_t startTime;
+    std::time_t endTime;
+    double wastedTime;
     bool progress = false;
 };
 #define CHECK_EXIT if (userInput == "exit") return 0;
-#define BASE_PROGRESS baseTask.back().progress
-#define BASE_NAME baseTask.back().name
-#define BASE_TIME_S baseTask.back().TimeS
-#define BASE_START_TIME baseTask.back().startTime
-#define BASE_TIME_E  baseTask.back().TimeE
-#define BASE_END_TIME baseTask.back().endTime
+#define TASK_PROGRESS baseTask.back().progress
+#define TASK_NAME baseTask.back().name
+#define TASK_TIME_S baseTask.back().startTime
+#define TASK_TIME_E  baseTask.back().endTime
+#define TASK_WASTED_TIME baseTask.back().wastedTime
 int main() {
     task strTask;
     std::vector<task> baseTask;
@@ -27,27 +25,37 @@ int main() {
         CHECK_EXIT
         if (userInput == "begin"){
             if(!baseTask.empty()){
-                if (BASE_PROGRESS){
-                    BASE_TIME_E = std::time(nullptr);
-                    BASE_END_TIME = std::localtime(&BASE_TIME_E);
-                    BASE_PROGRESS = false;
+                if (TASK_PROGRESS){
+                    TASK_TIME_E = std::time(nullptr);
+                    TASK_WASTED_TIME = std::difftime(TASK_TIME_E, TASK_TIME_S);
+                    TASK_PROGRESS = false;
                 }
             }
             std::cout << "Enter name task\n";
             std::cin >> userInput;
             baseTask.push_back(strTask);
-            BASE_PROGRESS = true;
-            BASE_NAME = userInput;
-            BASE_TIME_S = std::time(nullptr);
-            BASE_START_TIME = std::localtime(&BASE_TIME_S);
-            std::cout << "Added task " << "'" + BASE_NAME + "'"<< std::endl;
-            std::cout << "Start time " << asctime(BASE_START_TIME);
+            TASK_PROGRESS = true;
+            TASK_NAME = userInput;
+            TASK_TIME_S = std::time(nullptr);
+            std::cout << "Added task " << "'" + TASK_NAME + "'"<< std::endl;
+            std::cout << "Start time " << asctime(std::localtime(&TASK_TIME_S));
         }else if (userInput == "status"){
-            for (int i = 0; i < baseTask.size(); i++){
-                std::cout <<"'" + baseTask[i].name + "'"<< std::endl;
-                std::cout << "Start time " << asctime(baseTask[i].startTime) << std::endl;
-                if (!baseTask[i].progress) {
-                    std::cout << "End time " << asctime(baseTask[i].endTime) << std::endl;
+            for (auto & i : baseTask){
+                std::cout << std::endl;
+                std::cout <<"'" + i.name + "'"<< std::endl;
+                std::cout << "Start time\t" << asctime(std::localtime(&i.startTime));
+                if (!i.progress) {
+                    std::cout << "End time\t" << asctime(std::localtime(&i.endTime));
+                    std::cout << "Wasted time\t" << i.wastedTime / 3600 << std::endl;
+                }else std::cout << "task in progress\n";
+            }
+            std::cout << std::endl;
+        }else if (userInput == "end"){
+            for (auto & i : baseTask){
+                if (i.progress){
+                    i.endTime = std::time(nullptr);
+                    i.wastedTime = std::difftime(i.endTime, i.startTime);
+                    i.progress = false;
                 }
             }
         }
