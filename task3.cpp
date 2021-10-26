ba61950eb71a8e112c00446aa7cc7da52167f52a
stifler33 <bagi33.ru@gmail.com>
2021-10-26 22:16:58 +0300

    task3 finish

--- task3.cpp
+++ task3.cpp
@@ -1,4 +1,21 @@
-//
-// Created by bagi3 on 16.10.2021.
-//
-
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+int main(){
+    std::time_t null = 0;
+    std::tm timePause = *std::localtime(&null);
+    std::cin >> std::get_time(&timePause, "%M:%S");
+    std::time_t timePause_t = std::mktime(&timePause);
+    std::cout << std::put_time(&timePause, "%M:%S") << std::endl;
+    std::time_t a = std::time(nullptr);
+    while(timePause_t > 0){
+        if (a != std::time(nullptr)){
+            a = std::time(nullptr);
+            timePause_t --;
+            timePause = *std::localtime(&timePause_t);
+            std::cout << std::put_time(&timePause, "%M:%S") << std::endl;
+        }
+    }
+    std::cout << "DING! DING! DING!\n";
+    return 0;
+}
